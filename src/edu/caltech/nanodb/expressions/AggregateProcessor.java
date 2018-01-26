1b30fa0f5fbaca45c6e5dd17f4505d163afd65cd
Michelle Zhao <mzhao@caltech.edu>
2018-01-26 00:52:22 -0800

    Created AggregateProcessor class

---
+++ src/edu/caltech/nanodb/expressions/AggregateProcessor.java
@@ -0,0 +1,44 @@
+package edu.caltech.nanodb.expressions;
+import java.util.Collection;
+import java.util.HashMap;
+
+import edu.caltech.nanodb.queryast.SelectClause;
+import edu.caltech.nanodb.relations.ColumnInfo;
+import edu.caltech.nanodb.relations.Schema;
+import edu.caltech.nanodb.relations.SchemaNameException;
+import edu.caltech.nanodb.functions.AggregateFunction;
+import edu.caltech.nanodb.functions.Function;
+
+public class AggregateProcessor implements ExpressionProcessor {
+
+    int counter = 0;
+    /*PlanNode newNode = new HashedGroupAggregateNode(PlanNode subplan,
+            List<Expression> groupByExprs, Map<String, FunctionCall> aggregates)*/
+
+    HashMap<String, FunctionCall> aggregates = new HashMap<String, FunctionCall>();
+
+
+
+    public void enter(Expression node){
+        counter += 1;
+    }
+
+
+
+    public Expression leave(Expression node){
+        if(node instanceof FunctionCall)
+        {
+            FunctionCall call = (FunctionCall) node;
+            Function f = call.getFunction();
+            if (f instanceof AggregateFunction) {
+                String newName = "aggrfn%d" + counter;
+                aggregates.put(newName, call);
+                return new ColumnValue(new ColumnName(newName));
+
+            }
+        }
+        return node;
+    }
+
+}
+
